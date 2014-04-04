0cd7c549937432f7d26f4fce0738137ec164d7d6
caoguangayo <cgy8888@gmail.com>
2014-04-04 20:09:55 +0800

    add autocomplete for guzhangquery

---
+++ hsyw/App_Code/Entities/Area.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+/// <summary>
+///Area 的摘要说明
+/// </summary>
+public class Area
+{
+    //public string id   { get; set; }
+    public string code { get; set; }
+    public string name { get; set; }
+    public string parentCode { get; set; }
+    public string type { get; set; }
+    public string order { get; set; }
+    //public string isUse { get; set; }
+    //public string isVisible { get; set; }
+    public string isArea { get; set; }
+    public string path { get; set; }
+}
---
+++ hsyw/App_Code/ws.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using System.Data;
+
+/// <summary>
+///ws 的摘要说明
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+//若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消对下行的注释。 
+// [System.Web.Script.Services.ScriptService]
+public class ws : System.Web.Services.WebService {
+
+    public ws () {
+
+        //如果使用设计的组件，请取消注释以下行 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public void HelloWorld() {
+        string hello = "hello";
+        string world = "world";
+        ArrayList list = new ArrayList();
+        list.Add(hello);
+        list.Add(world);
+
+        string jsonData = JsonConvert.SerializeObject(list);
+
+        Context.Response.Write(jsonData);
+        Context.Response.End();
+        
+    }
+
+    //获得区域列表的json
+    [WebMethod]
+    public void get_area(string isArea)
+    {
+        string sql;
+        if (isArea.Length > 0)
+        {
+            sql = "select * from t_sys_branch b where b.ISUSE='1' and ISQY='1' order by DISPLAYORDER";
+        }
+        else {
+            sql = "select * from t_sys_branch b where b.ISUSE='1'  order by DISPLAYORDER";
+        }
+
+        DataSet ds = DataFunction.FillDataSet(sql);
+        ArrayList list = new ArrayList();
+        foreach (DataRow dr in ds.Tables[0].Rows)
+        {
+            Area a = new Area{
+                        name = dr["branchname"].ToString(),
+                        code = dr["branchcode"].ToString(),
+                        parentCode = dr["pbranchcode"].ToString(),
+                        type = dr["jglx_datadm"].ToString(),
+                        order =dr["displayorder"].ToString(),
+                        //isUse = dr["isuse"].ToString(),
+                        //isVisible = dr["isvisible"].ToString(),
+                        isArea = dr["isqy"].ToString(),
+                        path = dr["path"].ToString()};
+            list.Add(a);
+        }
+
+        ArrayList tmpList = new ArrayList();
+        foreach (Area a in list)
+        {
+            if (a.path.Length == 0)
+            {
+                a.path =string.Format(@"{0}/{1}", getParentPath(a.parentCode, list),a.name);
+            }
+
+            tmpList.Add(a.path);
+        }
+
+        list = tmpList;
+
+
+        string jsonData = JsonConvert.SerializeObject(list, Formatting.Indented);
+        Context.Response.Write(jsonData);
+        Context.Response.End();
+    
+    }
+
+    private string getParentPath(string parentCode,ArrayList list)
+    {
+        foreach (Area area in list)
+        {
+            string path = area.path;
+            string code = area.code;
+            if (code == parentCode) {
+                return path;
+            }
+        }
+        return null;
+    }
+    
+}
+
