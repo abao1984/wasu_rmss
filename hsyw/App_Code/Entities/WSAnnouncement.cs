0b99e9edf198e30eab88bfd8a1c6f461c70acd1a
caoguangayo <cgy8888@gmail.com>
2014-04-23 17:04:36 +0800

    为公告功能做的新web api

---
+++ hsyw/App_Code/Entities/WSAnnouncement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+/// <summary>
+///WSAnnouncement 的摘要说明
+/// </summary>
+public class WSAnnouncement
+{
+	public WSAnnouncement()
+	{
+		//
+		//TODO: 在此处添加构造函数逻辑
+		//
+	}
+
+    public int id { get; set; }
+    public string time { get; set; }
+    public string owner { get; set; }
+    public string title {get;set;}
+    public string content { get; set; }
+    public string comment { get; set; }
+
+}
