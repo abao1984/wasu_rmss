d6cd16f7c1dec609513d65843e29ee1ad1317e55
caoguangyao <cgy8888@gmail.com>
2014-06-20 16:33:10 +0800

    修改连接字符串

--- hsyw/App_Code/DataClasses.designer.cs
+++ hsyw/App_Code/DataClasses.designer.cs
@@ -68,7 +68,7 @@ public partial class DataClassesDataContext : System.Data.Linq.DataContext
   #endregion
 	
 	public DataClassesDataContext() : 
-			base(global::System.Configuration.ConfigurationManager.ConnectionStrings["RMSSConnectionString1"].ConnectionString, mappingSource)
+			base(global::System.Configuration.ConfigurationManager.ConnectionStrings["RMSSConnectionString"].ConnectionString, mappingSource)
 	{
 		OnCreated();
 	}
